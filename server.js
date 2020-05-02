ecc0d629d19fe29570866d6c85433b94459712c9
ego <ego@hackermail.com>
2020-05-02 18:15:46 +0630

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(express.static((__dirname, 'www')));
 
 let movies = require('./routes/movies');
 let category = require('./routes/category');
