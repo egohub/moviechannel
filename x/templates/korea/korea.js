7a08100316ea5192ce503425cb3802a31cfa40c6
ego <ego@hackermail.com>
2020-08-08 02:55:02 +0630

    Update korea.js

--- x/templates/korea/korea.js
+++ x/templates/korea/korea.js
@@ -12,19 +12,22 @@ function KoreaCtrl($scope, $log, $timeout, $ionicLoading, config, DataLoader) {
 
     $scope.loadPosts = function() {
         $ionicLoading.show({
-            template: '<ion-spinner icon="lines" class="spinner-assertive"></ion-spinner> ',
-            duration: 3000
+            template: '<ion-spinner icon="lines" class="spinner-assertive"></ion-spinner> '
+            // duration: 3000
         }).then(function() {
             console.log("The loading indicator is now displayed");
         });
         // Get all of our posts
-        DataLoader.get('/xxx/page/1').then(function(response) {
+        DataLoader.get('/api/sports').then(function(response) {
             $ionicLoading.hide();
             $scope.posts = response.data;
-
+            $scope.soccer = response.data.soccer;
+            $scope.basketball = response.data.basketball;
+            $scope.games = response.data.games;
+            $scope.tennis = response.data.tennis;
             $scope.moreItems = true;
 
-            console.log(response.data);
+            console.log(response.data.basketball);
 
         }, function(response) {
             console.log(response.data);
@@ -32,33 +35,33 @@ function KoreaCtrl($scope, $log, $timeout, $ionicLoading, config, DataLoader) {
 
     }
     $scope.loadPosts();
-    paged = 2;
-    $scope.loadMore = function() {
-        if (!$scope.moreItems) {
-            return;
-        }
-        var pg = paged++;
-        console.log('loadMore ' + pg);
-        $timeout(function() {
+    // paged = 2;
+    // $scope.loadMore = function() {
+    //     if (!$scope.moreItems) {
+    //         return;
+    //     }
+    //     var pg = paged++;
+    //     console.log('loadMore ' + pg);
+    //     $timeout(function() {
 
-            DataLoader.get('/xxx/page/' + pg).then(function(response) {
-                angular.forEach(response.data, function(value, key) {
-                    $scope.posts.push(value);
-                });
-                if (response.data.length <= 0) {
-                    $scope.moreItems = false;
-                }
-            }, function(response) {
-                $scope.moreItems = false;
-                $log.error(response);
-            });
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-            $scope.$broadcast('scroll.resize');
-        }, 1000);
-    }
-    $scope.moreDataExists = function() {
-        return $scope.moreItems;
-    }
+    //         DataLoader.get('/xxx/page/' + pg).then(function(response) {
+    //             angular.forEach(response.data, function(value, key) {
+    //                 $scope.posts.push(value);
+    //             });
+    //             if (response.data.length <= 0) {
+    //                 $scope.moreItems = false;
+    //             }
+    //         }, function(response) {
+    //             $scope.moreItems = false;
+    //             $log.error(response);
+    //         });
+    //         $scope.$broadcast('scroll.infiniteScrollComplete');
+    //         $scope.$broadcast('scroll.resize');
+    //     }, 1000);
+    // }
+    // $scope.moreDataExists = function() {
+    //     return $scope.moreItems;
+    // }
 
     $scope.doRefresh = function() {
         $timeout(function() {
@@ -71,13 +74,13 @@ function KoreaCtrl($scope, $log, $timeout, $ionicLoading, config, DataLoader) {
 
 function koreaWatchCtrl($scope, $stateParams, $ionicLoading, DataLoader, config) {
     $ionicLoading.show({
-        template: '<ion-spinner icon="lines" class="spinner-assertive"></ion-spinner> ',
-        duration: 3000
+        template: '<ion-spinner icon="lines" class="spinner-assertive"></ion-spinner> '
     }).then(function() {
         console.log("The loading indicator is now displayed");
     });
-    DataLoader.get('/xxx/' + $stateParams.id).then(function(resp) {
+    DataLoader.get('/api/sports/detail/' + $stateParams.id).then(function(resp) {
         $ionicLoading.hide();
         $scope.news = resp.data;
+        console.log(resp.data);
     });
 }
